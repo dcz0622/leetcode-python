71514d52d3ae4c3e94996232f78a4fec46839263
David Chen <david.chen@rakuten.com>
2020-05-25 03:37:23 +0800

    Upload 0001

---
+++ src/0001_two_sum.py
@@ -0,0 +1,28 @@
+"""
+PROBLEM DESCRIPTION
+
+Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+
+
+EXAMPLE
+
+- Input: nums = [2, 7, 11, 15], target = 9
+- Output: [0, 1]
+- Explanation: nums[0] + nums[1] = 2 + 7 = 9
+"""
+
+
+class Solution:
+  
+  def twoSum(self, nums: List[int], target: int) -> List[int]:
+    numToIdxDict = {}
+    
+    for i in range(len(nums)):
+      complement = target - nums[i]
+      
+      if complement not in numToIdxDict:
+        numToIdxDict[nums[i]] = i
+      else:
+        return [numToIdxDict[complement], i]
