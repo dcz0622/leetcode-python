689a84b6803cdba5853e0a4e6b258ba82e14c3d3
David Chen <david.chen@rakuten.com>
2020-05-26 21:09:25 +0800

    Upload 0004 | Problem description only

---
+++ src/0004_median_of_two_sorted_arrays.py
@@ -0,0 +1,21 @@
+"""
+PROBLEM DESCRIPTION
+
+There are two sorted arrays nums1 and nums2 of size m and n respectively. Find the median of the two sorted
+arrays. The overall run time complexity should be O(log (m+n)).
+
+You may assume nums1 and nums2 cannot be both empty.
+
+
+EXAMPLE 1
+
+- Input: nums1 = [1, 3], nums2 = [2]
+- Output: 2.0
+
+
+EXAMPLE 2
+
+- Input: nums1 = [1, 2], nums2 = [3, 4]
+- Output: 2.5
+"""
+
